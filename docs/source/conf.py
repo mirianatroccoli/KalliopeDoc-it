8b0d6656938d0de872576cb9cd5872489de2b190
mirianatroccoli <61968573+mirianatroccoli@users.noreply.github.com>
2023-02-09 16:09:08 +0100

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -3,8 +3,8 @@
 # -- Project information
 
 project = 'Kalliope'
-copyright = '2021, Graziella'
-author = 'Graziella'
+copyright = '2023, Kalliope'
+author = 'NetResults'
 
 release = '0.1'
 version = '0.1.0'
